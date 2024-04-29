47a4797d207f8508613d74a6525badc3c685327a
oyatrij <oyatrij@gmail.com>
2024-04-29 18:33:41 +0900

    first commit

---
+++ src/main/java/com/exam/swaggerexam/SwaggerExamApplication.java
@@ -0,0 +1,13 @@
+package com.exam.swaggerexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwaggerExamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exam/swaggerexam/SwaggerExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.swaggerexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerExamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
